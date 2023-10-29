3f958236daadf8b6f340b3686b55a080d2e9cf2a
Nick Alt <altnp291@gmail.com>
2023-10-29 16:03:25 -0400

    add utilities

---
+++ Utilities/InjectionInstanceProvider.cs
@@ -0,0 +1,84 @@
+﻿#if NETFRAMEWORK
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace Utilities
+{
+    public abstract class InjectionInstanceProvider : IInstanceProvider
+    {
+        private readonly Type _serviceType;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ConcurrentDictionary<InstanceContext, IServiceScope> _scopes;
+
+        public InjectionInstanceProvider(Type serviceType)
+        {
+            _serviceType = serviceType;
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+            _scopes = new ConcurrentDictionary<InstanceContext, IServiceScope>();
+        }
+
+        public object GetInstance(InstanceContext instanceContext)
+        {
+            var scope = _serviceProvider.CreateScope();
+            _scopes.TryAdd(instanceContext, scope);
+            return scope.ServiceProvider.GetRequiredService(_serviceType);
+        }
+
+        public object GetInstance(InstanceContext instanceContext, Message message)
+        {
+            return GetInstance(instanceContext);
+        }
+
+        public void ReleaseInstance(InstanceContext instanceContext, object instance)
+        {
+            _scopes.TryRemove(instanceContext, out var scope);
+            scope.Dispose();
+        }
+
+        public abstract void ConfigureServices(IServiceCollection serviceCollection);
+    }
+
+    public class InjectionServiceBehavior : Attribute, IServiceBehavior
+    {
+        public Type _injectionInstanceProviderType;
+        public InjectionServiceBehavior(Type injectionInstanceProviderType)
+        {
+            _injectionInstanceProviderType = injectionInstanceProviderType;
+        }
+
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+
+        }
+
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints,
+            BindingParameterCollection bindingParameters)
+        {
+
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            foreach (var cdb in serviceHostBase.ChannelDispatchers)
+            {
+                if (cdb is ChannelDispatcher cd)
+                {
+                    foreach (var ed in cd.Endpoints)
+                    {
+                        ed.DispatchRuntime.InstanceProvider = (InjectionInstanceProvider)Activator.CreateInstance(_injectionInstanceProviderType,
+                            new object[] { serviceDescription.ServiceType });
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ Utilities/ServiceProxyFactory.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+#if !NETFRAMEWORK
+using System.ServiceModel.Channels;
+using Microsoft.Extensions.Configuration;
+#endif
+
+namespace Tcetra.ServiceClient
+{
+    public class ServiceProxyFactory
+    {
+#if !NETFRAMEWORK
+        protected static readonly object ConfigLock = new object();
+        protected static IConfiguration Configuration;
+
+        public static void RegisterConfiguration(IConfiguration configuration)
+        {
+            lock (ConfigLock)
+            {
+                Configuration = configuration;
+            }
+        }
+#endif
+    }
+
+    public class ServiceProxyFactory<T> : ServiceProxyFactory
+        where T : class
+    {
+        internal static readonly object InitLock = new object();
+        internal static readonly object AbortLock = new object();
+
+        internal static ChannelFactory<T> ChannelFactory = null;
+
+        public static T CreateChannel()
+        {
+            InitializeChannelFactory();
+
+            T channel;
+            try
+            {
+                channel = ChannelFactory.CreateChannel();
+            }
+            catch (Exception ex)
+            {
+                lock (AbortLock)
+                {
+                    if (ChannelFactory != null)
+                    {
+                        try
+                        {
+                            ChannelFactory.Abort();
+                        }
+                        catch (Exception)
+                        { }
+                        ChannelFactory = null;
+                    }
+                }
+
+                throw new Exception("Exception creating channel from ChannelFactory.", ex);
+            }
+
+            return channel;
+        }
+
+        private static void InitializeChannelFactory()
+        {
+            if (ChannelFactory != null)
+                return;
+
+            lock (InitLock)
+            {
+                if (ChannelFactory != null)
+                    return;
+#if NETFRAMEWORK
+                ChannelFactory = new ChannelFactory<T>("*");
+
+#else
+                var configSection = $"WCFServices:{typeof(T).Name}";
+                var endpointUrl = Configuration[$"{configSection}:Endpoint"];
+
+                if (string.IsNullOrEmpty(endpointUrl))
+                    throw new ArgumentNullException($"{configSection}:Endpoint");
+
+                var section = Configuration.GetSection($"{configSection}:Binding");
+                var protocol = endpointUrl?.Split(":").FirstOrDefault();
+                Binding binding = protocol switch
+                {
+                    "http" => section.Get<BasicHttpBinding>() ?? new BasicHttpBinding { MaxReceivedMessageSize = int.MaxValue },
+                    "https" => section.Get<BasicHttpsBinding>() ?? new BasicHttpsBinding { MaxReceivedMessageSize = int.MaxValue },
+                    "net.tcp" => section.Get<NetTcpBinding>() ?? new NetTcpBinding(SecurityMode.None) { MaxReceivedMessageSize = int.MaxValue },
+                    _ => throw new NotSupportedException($"Unsupported Protocol '{protocol}'")
+                };
+
+                var endpointAddress = new EndpointAddress(endpointUrl);
+                ChannelFactory = new ChannelFactory<T>(binding, endpointAddress);
+#endif
+            }
+        }
+    }
+}
